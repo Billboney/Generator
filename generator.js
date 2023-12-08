3f6e62f061054b3aa49c2976f7af18092cfb51ef
Billboney <willharrison01@gmail.com>
2023-12-08 11:26:36 +0000

    added css box and constant variables that are random but don't change

--- generator.js
+++ generator.js
@@ -24,15 +24,31 @@ nouns = ['table', 'chair', 'door' ]
 
 adjectives = ['quick', 'smelly', 'bloated']
 
+names = ['minah', 'greg']
+
+adverbs = ['gleafuly', 'sadly', 'fappily']
+
 verb = () => verbs[Math.floor(Math.random() * verbs.length)]
+
+adverb = () => adverbs[Math.floor(Math.random() * adverbs.length)]
 
 noun = () => nouns[Math.floor(Math.random() * nouns.length)]
 
 adjective = () => adjectives[Math.floor(Math.random() * adjectives.length)]
 
+constNoun = nouns[Math.floor(Math.random() * nouns.length)]
+
+let nombre = names[Math.floor(Math.random() * names.length)]
 
 storys = {
-    storey1 : `this is a ${verb()} ${verb()} ${noun()} ${adjective()} test`
+    storey1 : `once apon a time there was a magical ${constNoun} that that could produce a constant stream
+               of ${adjective()} energy. clearly this ${adjective()} thing is too poweful to be left in
+               the wrong ${adjective()} hands. so the wizard ${nombre} picked up his ${noun()} and sealed it 
+               ${adverb()} destorying at least ${Math.floor(Math.random()*10000)} ${noun()}s in the process.
+               after idk like ${Math.floor(Math.random()*100)} years you gotta decide if you wanna use the ${constNoun}
+               yay? or Nay? ya gay` ,
+    storey2 : `this is yes`,
+    storey3 : `this is no`
 }
 
 console.log(storys.storey1)
