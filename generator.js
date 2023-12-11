b00ffbf5dc0f859d8c35977ebda3c3355af7734d
Billboney <willharrison01@gmail.com>
2023-12-11 11:43:06 +0000

    finished vesion 1

--- generator.js
+++ generator.js
@@ -19,36 +19,48 @@ maybe branching paths two choices each for different endings: 1 to 2 to 4
 
 
 verbs = ['run', 'walk', 'jog']
-
 nouns = ['table', 'chair', 'door' ]
-
 adjectives = ['quick', 'smelly', 'bloated']
-
 names = ['minah', 'greg']
-
 adverbs = ['gleafuly', 'sadly', 'fappily']
 
 verb = () => verbs[Math.floor(Math.random() * verbs.length)]
-
 adverb = () => adverbs[Math.floor(Math.random() * adverbs.length)]
-
 noun = () => nouns[Math.floor(Math.random() * nouns.length)]
-
 adjective = () => adjectives[Math.floor(Math.random() * adjectives.length)]
 
 constNoun = nouns[Math.floor(Math.random() * nouns.length)]
-
 let nombre = names[Math.floor(Math.random() * names.length)]
 
+let userNoun = prompt('enter a noun:')
+let userAdverb = prompt('enter a adverb:')
+let userAdjective = prompt('enter a adjective:')
+
+nouns.push(userNoun)
+adverbs.push(useradverb)
+adjectives.push(userAdjective)
+
+
 storys = {
-    storey1 : `once apon a time there was a magical ${constNoun} that that could produce a constant stream
+    story1 : `once apon a time there was a magical ${constNoun} that that could produce a constant stream
                of ${adjective()} energy. clearly this ${adjective()} thing is too poweful to be left in
                the wrong ${adjective()} hands. so the wizard ${nombre} picked up his ${noun()} and sealed it 
                ${adverb()} destorying at least ${Math.floor(Math.random()*10000)} ${noun()}s in the process.
                after idk like ${Math.floor(Math.random()*100)} years you gotta decide if you wanna use the ${constNoun}
                yay? or Nay? ya gay` ,
-    storey2 : `this is yes`,
-    storey3 : `this is no`
+    story2 : `this is yes`,
+    story3 : `this is no`
 }
 
-console.log(storys.storey1)
+console.log(storys.story1)
+
+choice = prompt('yes or no?:')
+while (choice != 'yes' && choice != 'no'){
+    choice = prompt('yes or no?:')
+}
+if(choice === 'yes'){
+    console.log(story.story2)
+} else if(choice === 'no'){
+        console.log(storys.story3)
+}
+
